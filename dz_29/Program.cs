bbae1db95491b5c425bfb50521a09f04e4b60b5c
Богдан <bogdandrenizhan@GMAIL.COM>
2023-05-31 22:36:15 +0300

    Дз_29

---
+++ dz_29/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+int[] GetArr (int n){
+    int[] arr = new int [8];
+    for(int i = 0; i < n; i++)
+    {  
+        arr[i] = new Random().Next (0, 100);
+    }
+    
+    return arr;
+}
+
+int [] arr = GetArr(8);
+
+for (int i = 0; i < 8; i++)
+{
+    Console.WriteLine($"[{String.Join("_", arr)}]");
+}
