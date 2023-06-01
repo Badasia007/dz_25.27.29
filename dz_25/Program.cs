0b4024586f6596661b764fe8179ddb87024bd6fb
Богдан <bogdandrenizhan@GMAIL.COM>
2023-06-01 23:06:41 +0300

    Дз_25

--- dz_25/Program.cs
+++ dz_25/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Введите число которое хотите возвести в степень:  ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите степень:  ");
+int number2 = int.Parse(Console.ReadLine());
+
+long StepenNumbers(int n, int n2){
+    int i = 1;
+    long result = 1;
+    while (number2 >= i)
+    {
+        result = result * number1;
+        i++;
+    }
+    return result;
+}
+Console.WriteLine($"Число {number1} в степени {number2} равно {StepenNumbers(number1, number2)}!");
